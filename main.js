23357f95436f72fa91a54c710414142ad2d88d0f
wwuZach <91570111+wwuZach@users.noreply.github.com>
2021-11-10 12:40:11 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: images
+      },
+      900
+    );
+  });
